9ae8c20ef1cea79311346cd4c820c4f44415102b
Javier Cervantes <1.27201@gmail.com>
2015-03-11 17:06:12 -0600

    Adding docs and making private funcs

--- weather/lib/weather/api.ex
+++ weather/lib/weather/api.ex
@@ -7,28 +7,35 @@ defmodule Weather.Api do
     "benghazi"    => "88319"
   }
 
+  @moduledoc """
+  Handles request and responses to REST weather service
+  """
+
+  @doc """
+  Takes city argument and returns a parse response tuple depending on result
+  """
   def fetch(city) do
     Logger.info "Fetching weather data for #{city}"
     city |> service_url |> HTTPoison.get() |> handle_response
   end
 
-  def service_url(city) do
+  defp service_url(city) do
     case @cities[city] do
       nil -> city_not_found(city)
       id  -> "#{Application.get_env(:weather, :service)}?id=#{id}"
     end
   end
 
-  def handle_response({ :ok, %HTTPoison.Response{ status_code: 200, body: body }}) do
+  defp handle_response({ :ok, %HTTPoison.Response{ status_code: 200, body: body }}) do
     { :ok, :jsx.decode(body) }
   end
 
-  def handle_response({ :ok, %HTTPoison.Response{ status_code: status, body: body }}) do
+  defp handle_response({ :ok, %HTTPoison.Response{ status_code: status, body: body }}) do
     Logger.error "Error #{status} returned"
     { :error, :jsx.decode(body) }
   end
 
-  def city_not_found(city) do
+  defp city_not_found(city) do
     IO.puts "#{city} city not found."
     IO.puts "Try one of this: #{inspect Map.keys(@cities)}"
     System.halt(2)
--- weather/lib/weather/cli.ex
+++ weather/lib/weather/cli.ex
@@ -2,11 +2,20 @@ defmodule Weather.CLI do
 
   @default_city "guadalajara"
 
+  @moduledoc """
+  Handle the command line parsing and the dispatch to the various
+  functions to print in screen the result of weather in  end to print
+  in screen the result of weather in guadalajara
+  """
+
+  @doc """
+  Main function that takes command line args and process them
+  """
   def main(argv) do
     argv |> parse_args |> process
   end
 
-  def parse_args(argv) do
+  defp parse_args(argv) do
     parse = OptionParser.parse(argv, switches: [ help: :boolean], aliases: [h: :help])
     case parse do
       { [ help: true ], _, _ } -> :help
@@ -15,27 +24,27 @@ defmodule Weather.CLI do
     end
   end
 
-  def process(:help) do
+  defp process(:help) do
     IO.puts """
     usage: weather [ city | #{@default_city} ]
     """
     System.halt(0)
   end
 
-  def process(city) do
+  defp process(city) do
     Weather.Api.fetch(city)
     |> decode_response
     |> print_result(city)
   end
 
-  def decode_response({ :ok, body }), do: body
-  def decode_response({ :error, error }) do
+  defp decode_response({ :ok, body }), do: body
+  defp decode_response({ :error, error }) do
     {_, message} = List.keyfind(error, "message", 0)
     IO.puts "Error fetching weather service: #{message}"
     System.halt(2)
   end
 
-  def print_result(list, city) do
+  defp print_result(list, city) do
     {"main", main} = List.keyfind(list, "main", 0)
     {"temp", temp} = List.keyfind(main, "temp", 0)
 
